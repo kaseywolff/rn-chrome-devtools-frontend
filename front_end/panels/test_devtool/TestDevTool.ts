e66b9b22fa2090c8378bb8349d624dab2e28f20d
Kasey Wolff <kasey.wolff@abbott.com>
2024-08-28 09:37:47 -0700

    test connection

--- front_end/panels/test_devtool/TestDevTool.ts
+++ front_end/panels/test_devtool/TestDevTool.ts
@@ -1,8 +1,25 @@
-import * as UI from '../../ui/legacy/legacy.js';
-import * as SDK from '../../core/sdk/sdk.js';
-import * as LitHtml from '../../ui/lit-html/lit-html.js';
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
+import type * as Common from '../../core/common/common.js';
+import * as UI from '../../ui/legacy/legacy.js';
+import * as Host from '../../core/host/host.js';
+import * as i18n from '../../core/i18n/i18n.js';
+import * as SDK from '../../core/sdk/sdk.js';
+
+import * as LitHtml from '../../ui/lit-html/lit-html.js';
+import type * as Platform from '../../core/platform/platform.js';
+import type * as Protocol from '../../generated/protocol.js';
+
+const UIStrings = {
+
+};
 const {render, html} = LitHtml;
+
+const str_ = i18n.i18n.registerUIStrings('panels/test_devtool/TestDevTool.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 
 declare global {
   interface Window {
@@ -10,16 +27,25 @@ declare global {
   }
 }
 
-export class TestDevTool extends UI.Widget.VBox implements SDK.TargetManager.SDKModelObserver<SDK.ReactNativeApplicationModel.ReactNativeApplicationModel> {
+declare global {
+  interface Window {
+    __TEST__?: any
+  }
+}
+
+export class TestDevTool extends UI.Widget.VBox implements
+  SDK.TargetManager.SDKModelObserver<SDK.ReactNativeApplicationModel.ReactNativeApplicationModel> {
+
   constructor() {
     super(true, true);
     this.render();
 
     SDK.TargetManager.TargetManager.instance().observeModels(SDK.ReactNativeApplicationModel.ReactNativeApplicationModel, this);
 
+    // Add code to receive message from RN app
     if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
       window.__REACT_DEVTOOLS_GLOBAL_HOOK__.on('customMessage', (message: any) => {
-        console.log('TEST DEVTOOL: message received', message);
+        console.log('TEST DEVTOOL: message received:', message);
       });
       console.log('TEST DEVTOOL: window.__REACT_DEVTOOLS_GLOBAL_HOOK__ exists:', window);
     } else {
